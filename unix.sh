fc07c44bc5f38fcf101e54d425a92bfb9b8f9e28
Matteo <mattegiorgi@gmail.com>
2024-09-06 10:43:56 +0200

    update

--- unix.sh
+++ unix.sh
@@ -66,7 +66,7 @@ warning-message
 SCRIPTPATH="$( cd "$(command dirname "$0")" ; pwd -P )" || exit 1
 "${SCRIPTPATH}/unix/fetch.sh"
 command sudo apt-get update && sudo apt-get upgrade -qq -y || error-echo "syncing repos"
-command sudo apt-get install -qq -y git xclip trash-cli bash bash-completion python3 python3-pip
+command sudo apt-get install -qq -y git xclip trash-cli bash bash-completion python3 python3-pip \
       vim-gtk3 tmux wamerican fd-find fzy htop fonts-firacode || error-echo "installing from apt"
 # ---
 if command cat /proc/version | grep -oE '[Dd]ebian' &>/dev/null || command cat /proc/version | grep -oE '[Uu]buntu' &>/dev/null; then
