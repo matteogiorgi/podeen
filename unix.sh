39ebe988d9c1c9b5bfc255630db78a709477e3ba
Matteo <mattegiorgi@gmail.com>
2024-09-08 22:15:00 +0200

    update

--- unix.sh
+++ unix.sh
@@ -72,8 +72,8 @@ command sudo apt-get install -qq -y git xclip trash-cli bash bash-completion pyt
 while read -p "$(echo -e "\n${RED}Would you like to install extras? (yes/no): ${NC}")" EXTRAS; do
     case "$EXTRAS" in
         [Yy] | [Yy][Ee][Ss])
-            command sudo apt-get install -qq -y diodon input-remapper xournalpp || error-echo "installing extras"
-            echo "Installed the following extras: input-remapper, diodon"
+            command sudo apt-get install -qq -y diodon flameshot input-remapper xournalpp || error-echo "installing extras"
+            echo "Installed the following extras: diodon, flameshot, input-remapper, xournalpp"
             break;;
         [Nn] | [Nn][Oo])
             echo "Skipping extras installation"
