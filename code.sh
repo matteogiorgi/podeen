c6ef4bb09824c709a98189919bc99cc7c6eb1411
Matteo <matteo.giorgi@protonmail.com>
2024-10-11 10:38:25 +0200

    rm pip

--- code.sh
+++ code.sh
@@ -39,7 +39,7 @@ SCRIPTPATH="$( cd "$(command dirname "$0")" ; pwd -P )" || exit 1
 "${SCRIPTPATH}/unix/fetch.sh"
 command sudo apt-get update && sudo apt-get upgrade -qq -y || error-echo "syncing repos"
 command sudo apt-get install -qq -y gnome-keyring git bash dash golang-go gopls python3 \
-      python3-pip black jupyter texlive-full fonts-firacode || error-echo "installing from apt"
+      black jupyter texlive-full fonts-firacode || error-echo "installing from apt"
 
 
 
